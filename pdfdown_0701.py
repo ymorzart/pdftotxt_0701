ed7a8929ede196093a0b84750c8a2efe69a85239
ymorzart <49669838+ymorzart@users.noreply.github.com>
2020-07-01 18:57:55 +0900

    Create pdfdown_0701.py
    pdfdown at website

---
+++ pdfdown_0701.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Tue Jul 01 14:03:06 2020
+
+@author: vincent.yu
+
+Download Web Site PDF file 
+SDS HomePage Download SDS Sustainability Report 2020
+"""
+import os
+
+import requests
+import shutil
+
+#res = requests.get('http://journals.sagepub.com/doi/pdf/10.1038/jcbfm.1993.48', stream=True, verify=False)
+res = requests.get('https://image.samsungsds.com/global/en/gnb/about/__icsFiles/afieldfile/2020/06/25/SamsungSDS_Sustainability_Report_2020_en_f.pdf?queryString=20200630070452', stream=True, verify=False)
+with open('K:/My files/Download/wordCloud/pdftotxt/abcd.pdf', 'wb') as f:
+    res.raw.decode_content = True
+    shutil.copyfileobj(res.raw, f)
